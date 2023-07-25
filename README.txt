ecb8af3ac20e5f4e6c03a87f6381ece52787b9fc
An Nguyen <anguyen.rain@gmail.com>
2023-07-25 01:23:41 -0700

    changed

--- README.txt
+++ README.txt
@@ -1,20 +1,39 @@
 - For front end, users go to local host. It will run index file. From there, users can input data for About page
 - For About page, input data such as weight, height, age, select gender. 
-- The front end just set up American units like feets, iches and pounds
-- On the About page, all values such as BMI, Body fat, idea weight, calories burn will be hidden. 
+
+- The front end just set up America units like feets, iches and pounds
+- To run fornt end, On the About page, all values such as BMI, Body fat, idea weight, calories burn will be hidden. 
 - After users hit the submit button, the server will response all values and show in the table and each section.
 - For each section, results will display color orange text.
 
-- For back-end, admin create the server to run API. In other word, API will run on node.js.
-- On the server node.js, I just use metric units such as metter and kg so I will convert American units on script.js.
-- In other word, convert unit before send to the server.
+////////////////////////////////////////////////
+- For back-end, created the server to run API. In other word, API will run on node.js.
+- On the server node.js, I just use metric units such as metter and kg so I will convert America units on script.js.
+- In other word, convert unit on script.js before send to the server.
 - In node.js, I run create functions to calucalte BMI, body fat, weight, burn calories. I resseached them on the Google
-- For script.js, I creat action for event listener and display result from respnose server. 
+- For script.js, I create action for event listener and display result from respnose server. 
+
+- To run Back-end, we need to open teminal and go to folder anguye9_a3. 
+- And then, use the command line "node node.js" to run the server. Now, the notification display on the terminal and make sure to display "Server is running on....." 
+- If the IDE requires install express, or cors, type on command "npm install express" or "npm install cors
+- After server run, go to the file index.html to run on browser.
+
+
+////////////////////////////////////////////////////
 
 Github address: https://github.com/anguyen5/anguye9_a3.git
+
 GCP link: http://34.83.229.172/ 
+****To run for GCP link, please edit local host API
+- Line 51: delete lolocal host "http://127.0.0.1:3000"
+- Line 52: delete lolocal host "http://127.0.0.1:3000"
+- Line 53: delete lolocal host "http://127.0.0.1:3000"
+- Line 54: delete lolocal host "http://127.0.0.1:3000"
+
 - I deployed on GCP, I used Bitnami and config on this link https://docs.bitnami.com/general/infrastructure/nodejs/get-started/get-started/
 - I used step 3 "Enable Predefined Virtual Hosts For An Express Application" with two command lines
-- 
+
+///////////////////////////////////////////////////
+Presentation
 - Because the file video is  heavy so I uploaded on youtube. 
 - Link presentation: https://www.youtube.com/watch?v=IJWTMVTN75E
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,133 +1,138 @@
 
 window.addEventListener('DOMContentLoaded', event => {
-   
-    // BMI
-    const form = document.getElementById('calculatorForm');
-    const statusDiv = document.getElementById('status');
 
-    async function callAPI(route, data) {
-        console.log("route: ", route);
+  // BMI
+  const form = document.getElementById('calculatorForm');
+  const statusDiv = document.getElementById('status');
 
-        const formData = new FormData(form);
-        console.log("formData", formData);
+  async function callAPI(route, data) {
+    console.log("route: ", route);
 
-        var height_m = (formData.get("height_ft") * 30.48 + formData.get("height_in") * 2.54) / 100.0;
-        var weight_kg = formData.get("weight") * 0.453592;
-        var age_ = formData.get("age");
-        var gender_ = formData.get("gender");
+    const formData = new FormData(form);
+    console.log("formData", formData);
 
-        data.height = height_m;
-        data.weight = weight_kg;
-        data.age = Number(age_);
-        data.gender= gender_;
+    var height_m = (formData.get("height_ft") * 30.48 + formData.get("height_in") * 2.54) / 100.0;
+    var weight_kg = formData.get("weight") * 0.453592;
+    var age_ = formData.get("age");
+    var gender_ = formData.get("gender");
 
-        console.log("data", data);
+    data.height = height_m;
+    data.weight = weight_kg;
+    data.age = Number(age_);
+    data.gender = gender_;
 
-        try {
-            const res = await fetch(route, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            });
+    console.log("data", data);
 
-            json = await res.json();
+    try {
+      const res = await fetch(route, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      });
 
-            console.log("API response: ", json);
-            //statusDiv.innerHTML = `<p>Result: ${JSON.stringify(json)}</p>`;
+      json = await res.json();
 
-            return json;
-        } catch (error) {
-            console.log("Error: ", error.message);
-            statusDiv.innerHTML = `<p>Error: ${error.message}</p>`;
-            return null;
-        }
+      console.log("API response: ", json);
+      //statusDiv.innerHTML = `<p>Result: ${JSON.stringify(json)}</p>`;
+
+      return json;
+    } catch (error) {
+      console.log("Error: ", error.message);
+      statusDiv.innerHTML = `<p>Error: ${error.message}</p>`;
+      return null;
+    }
+  }
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const bmiJSON = await callAPI(`http://127.0.0.1:3000/bmi`, {});
+    const bodyFatJSON = await callAPI(`http://127.0.0.1:3000/bodyfat`, { bmi: bmiJSON.bmi });
+    const weightJSON = await callAPI(`http://127.0.0.1:3000/idealweight`, { bmi: bmiJSON.idealWeight });
+    const calorieJSON = await callAPI(`http://127.0.0.1:3000/caloriesburned`, {});
+
+    //Run for GCP link
+    /*const bmiJSON = await callAPI(`/bmi`, {});
+    const bodyFatJSON = await callAPI(`/bodyfat`, { bmi: bmiJSON.bmi });
+    const weightJSON = await callAPI(`/idealweight`, { bmi: bmiJSON.idealWeight });
+    const calorieJSON = await callAPI(`/caloriesburned`, {});*/
+
+
+    $("#valueBMI").html("Your BMI is " + bmiJSON.bmi.toFixed(2));
+    $("#valueBodyfat").html("Your Body Fat is " + bodyFatJSON.bodyFat.toFixed(2) );
+    $("#valueBodyfatMean").html("valueBodyfatMean:  " + bodyFatJSON.notify);
+    $("#idealWeight").html("Value Idea Weight: " + (weightJSON.idealWeight * 2.20462).toFixed(2)+" lbs");
+    $("#calories").html("You need to Burn Calories: " + calorieJSON.caloriesBurned.toFixed(2));
+
+    const formData = new FormData(form);
+    var gender_ = formData.get("gender");
+    console.log("gender_ : " + gender_);
+    console.log("bodyFatJSON.bodyFat: ", bodyFatJSON.bodyFat);
+    var bodyFatCategory = "";
+
+    if (gender_ === "male") {
+      if (bodyFatJSON.bodyFat <= 8) {
+        document.getElementById("valueBodyfatMean").innerText = "Classification: Underfat";
+        bodyFatCategory = "Underfat";
+
+      } else if (bodyFatJSON.bodyFat < 20) {
+        document.getElementById("valueBodyfatMean").innerText = "Classification: Healthy";
+        bodyFatCategory = "Healthy";
+      } else if (bodyFatJSON.bodyFat < 25) {
+        document.getElementById("valueBodyfatMean").innerText = "Classification: Overfat";
+        bodyFatCategory = "Overfat";
+      } else {
+        document.getElementById("valueBodyfatMean").innerText = "Classification: Obese";
+        bodyFatCategory = "Obese";
+      }
+    }
+    else {
+      if (bodyFatJSON.bodyFat <= 21) {
+        document.getElementById("valueBodyfatMean").innerText = "Classification: Underfat";
+        bodyFatCategory = "Underfat";
+      } else if (bodyFatJSON.bodyFat < 33) {
+        document.getElementById("valueBodyfatMean").innerText = "Classification: Healthy";
+        bodyFatCategory = "Healthy";
+      } else if (bodyFatJSON.bodyFat < 39) {
+        document.getElementById("valueBodyfatMean").innerText = "Classification: Overfat";
+        bodyFatCategory = "Overfat";
+      } else {
+        document.getElementById("valueBodyfatMean").innerText = "Classification: Obese";
+        bodyFatCategory = "Obese";
+      }
     }
 
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const bmiJSON = await callAPI(`/bmi`, {});
-        const bodyFatJSON = await callAPI(`/bodyfat`, {bmi: bmiJSON.bmi});
-        const weightJSON = await callAPI(`/idealweight`, {bmi: bmiJSON.idealWeight});
-        const calorieJSON = await callAPI(`/caloriesburned`, {});
+    console.log("show table");
 
-      
+    $("#infoDialog").modal("show");
+    // map response data to HTML elements in modal
+    $("#valueBMI_").html("BMI:  " + bmiJSON.bmi.toFixed(2));
+    $("#valueBodyfat_").html("Body Fat: " + bodyFatJSON.bodyFat.toFixed(2));
+    $("#valueBodyfatMean_").html("Classification:  " + bodyFatCategory);
+    $("#idealWeight_").html("Value Idea Weight (Lbs):  " + (weightJSON.idealWeight * 2.20462).toFixed(2));
+    $("#calories_").html("Calories Burn : " + calorieJSON.caloriesBurned.toFixed(2));
 
-        $("#valueBMI").html("Your BMI is " + bmiJSON.bmi.toFixed(2));
-        $("#valueBodyfat").html("Your Body Fat is " + bodyFatJSON.bodyFat.toFixed(2));
-        $("#valueBodyfatMean").html("valueBodyfatMean:  " + bodyFatJSON.notify);
-        $("#idealWeight").html("Value Idea Weight: " + (weightJSON.idealWeight*2.20462).toFixed(2));
-        $("#calories").html("You need to Burn Calories: " + calorieJSON.caloriesBurned.toFixed(2));
+    // retrieve modal
+    var modal = document.getElementById("infoDialog");
 
-        const formData = new FormData(form);
-        var gender_ = formData.get("gender");
-        console.log("gender_ : "+ gender_);
-        console.log("bodyFatJSON.bodyFat: " , bodyFatJSON.bodyFat);
-        var bodyFatCategory = "";
+    // retrieve span when closing the modal
+    // var span = document.getElementsByClassName("close")[0];
 
-        if (gender_ === "male") {
-            if (bodyFatJSON.bodyFat <= 8) {
-              document.getElementById("valueBodyfatMean").innerText = "Classification: Underfat";
-              bodyFatCategory = "Underfat";
+    // if the user clicks on the closing icon (x), then clear the modal
+    // span.onclick = function () {
+    //     modal.style.display = "none";
+    // };
 
-            } else if (bodyFatJSON.bodyFat < 20) {
-              document.getElementById("valueBodyfatMean").innerText = "Classification: Healthy";
-              bodyFatCategory = "Healthy";
-            } else if (bodyFatJSON.bodyFat < 25) {
-              document.getElementById("valueBodyfatMean").innerText = "Classification: Overfat";
-              bodyFatCategory = "Overfat";
-            } else {
-              document.getElementById("valueBodyfatMean").innerText = "Classification: Obese";
-              bodyFatCategory = "Obese";
-          }
-        }
-        else{
-            if (bodyFatJSON.bodyFat <= 21) {
-                document.getElementById("valueBodyfatMean").innerText = "Classification: Underfat";
-                bodyFatCategory = "Underfat";
-              } else if (bodyFatJSON.bodyFat < 33) {
-                document.getElementById("valueBodyfatMean").innerText = "Classification: Healthy";
-                bodyFatCategory = "Healthy";
-              } else if (bodyFatJSON.bodyFat < 39) {
-                document.getElementById("valueBodyfatMean").innerText = "Classification: Overfat";
-                bodyFatCategory = "Overfat";
-              } else {
-                document.getElementById("valueBodyfatMean").innerText = "Classification: Obese";
-                bodyFatCategory = "Obese";
-            }
-        }
-
-        console.log("show table");
-
-        $("#infoDialog").modal("show");
-         // map response data to HTML elements in modal
-         $("#valueBMI_").html("BMI:  " +bmiJSON.bmi.toFixed(2));
-         $("#valueBodyfat_").html("Body Fat:  " + bodyFatJSON.bodyFat.toFixed(2));
-         $("#valueBodyfatMean_").html("Classification:  " +bodyFatCategory);
-         $("#idealWeight_").html("Value Idea Weight:  " + (weightJSON.idealWeight*2.20462).toFixed(2));
-         $("#calories_").html("Calories Burn:  " + calorieJSON.caloriesBurned.toFixed(2));
-
-        // retrieve modal
-        var modal = document.getElementById("infoDialog");
-
-        // retrieve span when closing the modal
-        // var span = document.getElementsByClassName("close")[0];
-
-        // if the user clicks on the closing icon (x), then clear the modal
-        // span.onclick = function () {
-        //     modal.style.display = "none";
-        // };
-
-        // if the user clicks anywhere on the page outside modal boundary, then clear the modal
-        window.onclick = function (event) {
-            if (event.target === modal) {
-                modal.style.display = "none";
-            }
-        };
-
-       
+    // if the user clicks anywhere on the page outside modal boundary, then clear the modal
+    window.onclick = function (event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    };
 
 
-    });
+
+
+  });
 })
